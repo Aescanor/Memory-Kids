9509330082db28ab8af1da5d95699f83aca179f6
Æscanor <113015001+Aescanor@users.noreply.github.com>
2023-03-07 11:25:13 +0100

    starting js

---
+++ index.js
@@ -0,0 +1,132 @@
+// computer:
+const computerScore = document.querySelector(".computer-score");
+
+// general:
+const infoParty = document.querySelector(".info-party");
+
+// gaming board :
+const gamingBoard = document.querySelector(".gaming-board");
+
+// cards 
+// change of color when the computer or the player wins the card :
+const card = document.querySelectorAll(".flip-card")[0];
+
+// for random images :
+const cardImage = document.querySelectorAll(".flip-card-back");
+
+
+// player side:
+// playerTag: 
+const pseudo = document.querySelector(".pseudo");
+const inpPlayerTag = document.querySelector("input.playerTag");
+const btnPlayerTag = document.querySelector(".btn-playerTag");
+// score:
+const playerScore = document.querySelector("span.player-score");
+
+// // I - array of images : 
+
+const imagesTab = [
+
+    {
+        id: 0,
+        url: "autruche.png"
+    },
+
+    {
+        id: 1,
+        url: "boa.png"
+    },
+
+    {
+        id: 2,
+        url: "crocodile.png"
+    },
+
+    {
+        id: 3,
+        url: "elephant.png"
+    },
+
+    {
+        id: 4,
+        url: "iguane.png"
+    },
+
+    {
+        id: 5,
+        url: "kangourou.png"
+    },
+
+    {
+        id: 6,
+        url: "koala.png"
+    },
+
+    {
+        id: 7,
+        url: "leopard.png"
+    },
+
+    {
+        id: 8,
+        url: "lion.png"
+    },
+
+
+    {
+        id: 9,
+        url: "ours.png"
+    },
+
+    {
+        id: 10,
+        url: "panda.png"
+    },
+
+    {
+        id: 11,
+        url: "panthere.png"
+    },
+
+    {
+        id: 12,
+        url: "renard.png"
+    },
+
+
+    {
+        id: 13,
+        url: "rhinoceros.png"
+    },
+
+    {
+        id: 14,
+        url: "tiger.png"
+    },
+
+    {
+        id: 15,
+        url: "zebra.png"
+    }
+]
+
+
+
+
+
+// II - Function Shuffle :
+
+
+
+
+
+
+// III - Selected Card :
+
+
+
+// IV - Timer ? :
+
+
+// V - Function WinnerIs :
+
