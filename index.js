ed37217f26c66026476093066e54c38f3726409b
Æscanor <113015001+Aescanor@users.noreply.github.com>
2023-03-07 17:24:18 +0100

    start function shuffle

--- index.js
+++ index.js
@@ -25,108 +25,48 @@ const playerScore = document.querySelector("span.player-score");
 
 // // I - array of images : 
 
-const imagesTab = [
-
-    {
-        id: 0,
-        url: "autruche.png"
-    },
-
-    {
-        id: 1,
-        url: "boa.png"
-    },
-
-    {
-        id: 2,
-        url: "crocodile.png"
-    },
-
-    {
-        id: 3,
-        url: "elephant.png"
-    },
-
-    {
-        id: 4,
-        url: "iguane.png"
-    },
-
-    {
-        id: 5,
-        url: "kangourou.png"
-    },
-
-    {
-        id: 6,
-        url: "koala.png"
-    },
-
-    {
-        id: 7,
-        url: "leopard.png"
-    },
-
-    {
-        id: 8,
-        url: "lion.png"
-    },
-
-
-    {
-        id: 9,
-        url: "ours.png"
-    },
-
-    {
-        id: 10,
-        url: "panda.png"
-    },
-
-    {
-        id: 11,
-        url: "panthere.png"
-    },
-
-    {
-        id: 12,
-        url: "renard.png"
-    },
-
-
-    {
-        id: 13,
-        url: "rhinoceros.png"
-    },
-
-    {
-        id: 14,
-        url: "tiger.png"
-    },
-
-    {
-        id: 15,
-        url: "zebra.png"
-    }
+const animaux = [
+ './public/images/autruche.png', './public/images/autruche.png',
+ '/public/images/boa.png', '/public/images/boa.png',
+ './public/images/crocodile.png', './public/images/crocodile.png',
+ './public/images/elephant.png', './public/images/elephant.png',
+ './public/images/iguane.png', './public/images/iguane.png',
+ './public/images/kangourou.png', './public/images/kangourou.png',
+ './public/images/koala.png', './public/images/koala.png',
+ './public/images/leopard.png', './public/images/leopard.png',
+ './public/images/lion.png', './public/images/lion.png',
+ './public/images/ours.png', './public/images/ours.png',
+ './public/images/panda.png', './public/images/panda.png',
+ './public/images/panthere.png', './public/images/panthere.png',
+ './public/images/renard.png', './public/images/renard.png',
+ './public/images/rhinoceros.png', './public/images/rhinoceros.png',
+ './public/images/tigre.png', './public/images/tigre.png',
+ './public/images/zebre.png', './public/images/zebre.png',
 ]
 
+// TEST /
+const backCard = document.querySelector(".flip-card-back")
 
-
+backCard.innerHTML = `<img src="${animaux[0]}" alt="fruit">`
 
 
 // II - Function Shuffle :
 
 
 
+// // III - Selected Card :
 
 
 
-// III - Selected Card :
+// // IV - Timer ? :
 
 
+// // V - Function WinnerIs :
 
-// IV - Timer ? :
+
+// // features: 
+
+// // PlayerTag : 
 
 
-// V - Function WinnerIs :
 
---
+++ javascript/animaux.js
@@ -0,0 +1,23 @@
+
+// // I - array of images : 
+
+const animaux = [
+    './public/imges/autruche.png', './public/imges/autruche.png',
+    './public/imges/boa.png', './public/imges/boa.png',
+    './public/imges/crocodile.png', './public/imges/crocodile.png',
+    './public/imges/elephant.png', './public/imges/elephant.png',
+    './public/imges/iguane.png', './public/imges/iguane.png',
+    './public/imges/kangourou.png', './public/imges/kangourou.png',
+    './public/imges/koala.png', './public/imges/koala.png',
+    './public/imges/leopard.png', './public/imges/leopard.png',
+    './public/imges/lion.png', './public/imges/lion.png',
+    './public/imges/ours.png', './public/imges/ours.png',
+    './public/imges/panda.png', './public/imges/panda.png',
+    './public/imges/panthere.png', './public/imges/panthere.png',
+    './public/imges/renard.png', './public/imges/renard.png',
+    './public/imges/rhinoceros.png', './public/imges/rhinoceros.png',
+ './public/imges/tigre.png', './public/imges/tigre.png',
+ './public/imges/zebre.png', './public/imges/zebre.png',
+]
+
+export{animaux}
